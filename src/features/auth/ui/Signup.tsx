75905fdae62f5a7104d2d66f26d83af409a3fbe1
wlstn <wlstn159159@naver.com>
2025-08-07 21:59:22 +0900

    feat: 회원가입 폼 및 API 요청 기능 구현 및 폼 제출 이벤트 처리 추가

--- src/features/auth/ui/Signup.tsx
+++ src/features/auth/ui/Signup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Button,
   Field,
@@ -7,36 +9,56 @@ import {
   Heading,
 } from "@chakra-ui/react";
 
+import { postSignup } from "../api/postSignup";
+
 const Signup = () => {
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // 새로고침 방지
+    const res = await postSignup({ userId: userId, password: password });
+    console.log(res);
+  };
+
   return (
-    <Flex minH="100vh" align="center" justify="center">
-      <Fieldset.Root size="lg" maxW="md">
-        <Flex justify="center">
-          <Heading size="4xl">회원가입</Heading>
-        </Flex>
+    <form onSubmit={handleSubmit}>
+      <Flex minH="100vh" align="center" justify="center">
+        <Fieldset.Root size="lg" maxW="md">
+          <Flex justify="center">
+            <Heading size="4xl">회원가입</Heading>
+          </Flex>
 
-        <Fieldset.Content>
-          <Field.Root>
-            <Field.Label>아이디</Field.Label>
-            <Input name="name" />
-          </Field.Root>
+          <Fieldset.Content>
+            <Field.Root>
+              <Field.Label>아이디</Field.Label>
+              <Input
+                name="userId"
+                onChange={(e) => setUserId(e.target.value)}
+              />
+            </Field.Root>
 
-          <Field.Root>
-            <Field.Label>비밀번호</Field.Label>
-            <Input name="password" type="password" />
-          </Field.Root>
+            <Field.Root>
+              <Field.Label>비밀번호</Field.Label>
+              <Input
+                name="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Field.Root>
 
-          <Field.Root>
-            <Field.Label>비밀번호 확인</Field.Label>
-            <Input name="password-check" type="password" />
-          </Field.Root>
-        </Fieldset.Content>
+            <Field.Root>
+              <Field.Label>비밀번호 확인</Field.Label>
+              <Input name="password-check" type="password" />
+            </Field.Root>
+          </Fieldset.Content>
 
-        <Button w="full" type="submit" alignSelf="flex-start">
-          회원가입
-        </Button>
-      </Fieldset.Root>
-    </Flex>
+          <Button w="full" type="submit" alignSelf="flex-start">
+            회원가입
+          </Button>
+        </Fieldset.Root>
+      </Flex>
+    </form>
   );
 };
 
