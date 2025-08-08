044f20109ea26375398dcd4b21987664e7214b32
wlstn <wlstn159159@naver.com>
2025-08-08 14:32:15 +0900

    chore: axios 인스턴스에 withCredentials 설정 추가
    백엔드에서 내려주는 쿠키를 브라우저에 저장하기 위해 axios 설정 수정

--- src/features/auth/api/client.tsx
+++ src/features/auth/api/client.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 // 인스턴스를 생성할때 config 기본값 설정하기
 const client = axios.create({
-  baseURL: SERVER_URL,
+    baseURL: SERVER_URL,
+    withCredentials: true, //
 });
 
 export default client;
