146760cc76316dd9d3c070c62fb8f24d7e2af88a
wlstn <wlstn159159@naver.com>
2025-08-08 23:22:28 +0900

    refactor: 기본 경로를 "/" 로 변경

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter(
             element: <Chat />,
         },
     ],
-    { basename: "/chat" },
+    { basename: "/" },
 );
 
 export default router;
--- src/features/auth/api/postLogin.tsx
+++ src/features/auth/api/postLogin.tsx
@@ -1,11 +1,11 @@
-import client from "./client";
+import client from "../../shared/client";
 
 interface loginData {
-  userId: String;
-  password: String;
+    userId: String;
+    password: String;
 }
 
 export const postLogin = async (data: loginData) => {
-  const response = await client.post("/login", data);
-  return response;
+    const response = await client.post("/auth/login", data);
+    return response;
 };
